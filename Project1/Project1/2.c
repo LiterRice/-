72e40347b81ee3b793b845aaa4d95c8e80818bb5
rise <qq1303672788@gmail.com>
2022-04-29 23:23:23 +0800

    测试

---
+++ Project1/Project1/2.c
@@ -0,0 +1,10 @@
+#include  <stdio.h>
+
+//extern int g_val;
+//
+//int main()
+//{
+//	printf("%d", g_val);
+//
+//	return 0;
+//}
---
+++ Project1/Project1/源.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int g_val = 2022;
+
+//int main()
+//{
+//	printf("hehe\n");
+//	int a = 10;
+//	printf("a = %d\n", a);
+//	
+//	return 0;
+//}
